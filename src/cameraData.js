c4a2c32002cd644292f4b2ea43d59ddadc25906b
sryvprasad <sryvprasad@gmail.com>
2025-01-29 13:17:57 +1100

    iframe link changed

--- src/cameraData.js
+++ src/cameraData.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
