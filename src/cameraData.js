76f305df25349542983d4e9deed3bb0b35dabce5
sryvprasad <sryvprasad@gmail.com>
2025-01-29 11:36:22 +1100

    new version

--- src/cameraData.js
+++ src/cameraData.js
@@ -1,9 +1,6 @@
 import * as THREE from "three";
-
-import { DeviceOrientationControls } from "./jsm/controls/DeviceOrientationControls.js";
-
-
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { DeviceOrientationControls } from "./jsm/controls/DeviceOrientationControls.js";
 
 const requestAccessButton = document.getElementById("request-access");
 const gyroDataDiv = document.getElementById("gyro-data");
@@ -11,15 +8,14 @@ const gyroDataDiv = document.getElementById("gyro-data");
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-const domainURL = "https://3dgyroscope.netlify.app/";
 camera.position.z = 5;
 
 const orbitControls = new OrbitControls(camera, renderer.domElement);
@@ -76,9 +72,9 @@ function sendDataToIframe() {
   if (iframe && iframe.contentWindow) {
     const data = {
       rotation: {
-        x: cube.rotation.x,
-        y: cube.rotation.y,
-        z: cube.rotation.z,
+        x: camera.rotation.x,
+        y: camera.rotation.y,
+        z: camera.rotation.z,
       },
       position: {
         x: camera.position.x,
@@ -86,24 +82,20 @@ function sendDataToIframe() {
         z: camera.position.z,
       },
     };
-    console.log('data', data)
+    console.log('Sending data to iframe:', data);
     iframe.contentWindow.postMessage({ type: 'data', ...data }, "*");
-    // Send data to the iframe
-    // iframe.contentWindow.postMessage(data, "*");
   }
 }
 
 function animate() {
   requestAnimationFrame(animate);
 
-  orbitControls.update();
   if (deviceOrientationControls) {
     deviceOrientationControls.update();
   } else {
     orbitControls.update();
   }
 
-  // Send camera position and rotation to the iframe
   sendDataToIframe();
   renderer.render(scene, camera);
 }
